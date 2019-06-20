b145a3c5bfad2337572dde56ed7c928a7ecd8b6a
Dawid Pereira <pereiradawid@outlook.com>
2019-06-20 20:26:25 +0200

    Two Sum

---
+++ LeetCode/Easy/Two_Sum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public class Two_Sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            if (nums?.Length < 2)
+            {
+                return null;
+            }
+
+            var checkedValue = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var currentValue = nums[i];
+                var searchValue = target - currentValue;
+
+                if (checkedValue.ContainsKey(searchValue))
+                {
+                    return new int[2]
+                    {
+                        checkedValue[searchValue],
+                        i
+                    };
+                }
+
+                if (checkedValue.ContainsKey(currentValue) == false)
+                {
+                    checkedValue.Add(currentValue, i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
